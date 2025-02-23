3e71bc23a5b24f505921727d9bf80882cce5cb65
7vtr0x7 <7vtrox7@gmail.com>
2025-02-23 17:06:58 +0530

    q3

---
+++ q3.js
@@ -0,0 +1,19 @@
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  const obj1 = {};
+  const obj2 = {};
+
+  for (let i = 0; i < s.length; i++) {
+    obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+    obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+  }
+
+  for (let key in obj1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+
+  return true;
+};
+
+console.log(isAnagram("car", "rac"));

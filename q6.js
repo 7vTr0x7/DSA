48c4f2794d7a90d77368a44ad35e3432832be3bb
7vtr0x7 <7vtrox7@gmail.com>
2025-02-23 17:44:39 +0530

    q6

---
+++ q6.js
@@ -0,0 +1,23 @@
+//Best Time to Buy and Sell Stock
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+const maxProfit = (prices) => {
+  let min = prices[0];
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
+    }
+    if (prices[i] - min > profit) {
+      profit = prices[i] - min;
+    }
+  }
+
+  return profit;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));

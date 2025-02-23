98b3c20938c3cccc6d401d8e2a22f2a0535ea85a
7vtr0x7 <7vtrox7@gmail.com>
2025-02-23 17:59:29 +0530

    q6

--- q6.js
+++ q6.js
@@ -12,10 +12,12 @@ const maxProfit = (prices) => {
     if (prices[i] < min) {
       min = prices[i];
     }
-    profit = Math.max(profit, prices[i] - min);
+    if (prices[i] - min > profit) {
+      profit = prices[i] - min;
+    
   }
 
   return profit;
 };
 
-console.log(maxProfit([7, 1, 5, 3, 6, 4, 10]));
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
